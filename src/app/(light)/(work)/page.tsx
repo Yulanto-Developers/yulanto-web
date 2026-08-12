b119746c7f7986100c21f29aadc89bbc1c3d3e37
Yulanto-Developers <yulantodevelopers@gmail.com>
2026-08-12 10:41:18 +0530

    extra spae romved

---
+++ src/app/(light)/(work)/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+  title: "Yulanto - Home",
+};
+
+const page= ()=>{
+    return(
+        <>
+        
+        </>
+    )
+}
