2d282e9bb7e69c2369b99d7f481c68e63e14c7a1
Yulanto-Developers <yulantodevelopers@gmail.com>
2026-08-18 19:20:51 +0530

    ak

--- src/components/breadcrum/sections/breadcrumbdata.tsx
+++ src/components/breadcrum/sections/breadcrumbdata.tsx
@@ -12,6 +12,31 @@ const data = [
     backgroundImage: "/assets/images/breadcrumb/Company-Overview.jpg",
   },
   {
+    path: '/our-team',
+    title: "Meet Our Team",
+    currentPage: "Meet Our Team",
+    backgroundImage: "/assets/images/breadcrumb/Company-Overview.jpg",
+  },
+  {
+    path: '/our-mission-vision',
+    title: "Our Vision & Mission",
+    currentPage: "Our Vision & Mission",
+    backgroundImage: "/assets/images/breadcrumb/Company-Overview.jpg",
+  }
+  ,
+  {
+    path: '/our-philosophy',
+    title: "Our Philosophy",
+    currentPage: "Our Philosophy",
+    backgroundImage: "/assets/images/breadcrumb/Company-Overview.jpg",
+  },
+  {
+    path: '/why-choose-us',
+    title: "Why Choose Us",
+    currentPage: "Why Choose Us",
+    backgroundImage: "/assets/images/breadcrumb/Company-Overview.jpg",
+  },
+  {
     path: "/clients",
     title: "Our Clients",
     currentPage: "Our Clients",
