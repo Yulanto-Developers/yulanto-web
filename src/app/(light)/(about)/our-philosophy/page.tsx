2117ddf8f92b36c6058989053796679c6fd2eb0d
Yulanto-Developers <yulantodevelopers@gmail.com>
2026-08-21 16:31:13 +0530

    Fix ourPhilosophy folder casing

--- src/app/(light)/(about)/our-philosophy/page.tsx
+++ src/app/(light)/(about)/our-philosophy/page.tsx
@@ -1,6 +1,6 @@
 import Breadcrumbdata from "@/components/breadcrum/sections/breadcrumbdata";
 import { Metadata } from "next";
-import GradientCardDemo from "@/components/home/home/ourphilosophy/GradientCardDemo";
+import GradientCardDemo from "@/components/home/home/ourPhilosophy/GradientCardDemo";
 
 export const metadata: Metadata = {
   title: "Home - Creative Portfolio Nextjs Template",
